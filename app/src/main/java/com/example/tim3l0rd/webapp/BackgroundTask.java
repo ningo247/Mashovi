054396fef576ae242ef48d6f279d526e33aa35eb
ningo247 <oxygen24@gmail.com>
2016-01-01 09:57:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tim3l0rd/webapp/BackgroundTask.java
@@ -0,0 +1,72 @@
+package com.example.tim3l0rd.webapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by Tim3L0rd on 12/29/2015.
+ */
+public class BackgroundTask extends AsyncTask<String,Void,String> {
+    Context ctx;
+
+    BackgroundTask(Context ctx){
+        this.ctx = ctx;
+    }
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String insert_url = "http://http://myposts.netau.net/webapp/insert.php";
+        String method = params[0];
+        if(method.equals("send_data")){
+
+            String data = params[1];
+            try {
+                URL url = new URL(insert_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                String data_to_write = URLEncoder.encode("data","UTF-8")+"="+URLEncoder.encode(data,"UTF-8");
+                bufferedWriter.write(data_to_write);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                os.close();
+                InputStream IS = httpURLConnection.getInputStream();
+                IS.close();
+                return "Data added Successfuly!";
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(ctx,result,Toast.LENGTH_LONG).show();
+    }
+}
